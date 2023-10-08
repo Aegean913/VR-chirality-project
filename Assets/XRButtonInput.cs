f9b4adfb4fdf7ef6ba12375a0ff54870235175a1
Aegean913 <34204256+Aegean913@users.noreply.github.com>
2023-10-08 14:25:34 +1100

    fixed sizing and positions

---
+++ Assets/XRButtonInput.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class XRButtonInput : MonoBehaviour
+{
+    public InputAction action; // Declares action to be set in editor
+    public UnityEvent inputPress; // Declares event from which methods can be called
+
+
+}
